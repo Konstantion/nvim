61afcac4a3caaa062af9a5d28178a41b21d6481b
Konstantion <yagudinkostya@gmail.com>
2026-04-27 23:19:42 +0300

    feat: add plugin to render markdawn

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -97,4 +97,16 @@ return {
 			vim.cmd("colorscheme github_dark_high_contrast")
 		end,
 	},
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {},
+		config = function()
+			require("render-markdown").setup({
+                enabled = false
+            })
+		end,
+	},
 }
