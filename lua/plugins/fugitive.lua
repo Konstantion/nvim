a1456161607d29b262e8652ed0991ae4eb119440
Konstantion <yagudinkostya@gmail.com>
2026-07-01 12:15:15 +0300

    fix: initialize fugitive eagerly

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,5 +1,6 @@
 return {
     "tpope/vim-fugitive",
+    cmd = { "Git", "G", "Gdiffsplit", "Gread", "Gwrite", "Ggrep", "GMove", "GDelete", "GBrowse" },
     keys = {
         { "<leader>gs", vim.cmd.Git, desc = "Git Status" },
         -- 3-way merge conflict resolution
